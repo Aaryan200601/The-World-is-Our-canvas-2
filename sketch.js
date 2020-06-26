6d83d779195b94b68373640c98acb3e212fc74aa
Aaryan200601 <65025065+Aaryan200601@users.noreply.github.com>
2020-06-26 19:52:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var database;
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
+
+function setup() {
+  database = firebase.database();
+  var canvas = createCanvas(400,400);
+  canvas.mousePressed(startPath);
+  canvas.mouseReleased(endPath);
+  canvas.parent('canvascontainer');
+}
+
+function draw() {
+  background(0);  
+
+  if(isDrawing)
+  {
+    var point = {
+      x:mouseX,
+      y:mouseY
+    }
+    currentPath.push(point);
+  }
+
+  strokeWeight(3);
+  stroke(255);
+  noFill();
+  for(var i = 0; i < drawing.length; i++) {
+    var path = drawing[i];
+    beginShape();
+    for(var j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y);
+    }
+    endShape();
+  }
+}
+
+function startPath()
+{
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath()
+{
+  isDrawing = false;
+}
+
+function upload()
+{
+  var GameStateRef =database.ref('drawing');
+  GameStateRef.on("value", function(data){
+    drawing = data.val(); 
+  })
+}
